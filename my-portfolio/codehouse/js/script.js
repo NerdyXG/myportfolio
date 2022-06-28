2fd0ecb9fdbd3f985f9cd6dffe878d62f3fa346a
NerdyXG <108344292+NerdyXG@users.noreply.github.com>
2022-06-28 20:28:54 +0100

    Add files via upload

---
+++ my-portfolio/codehouse/js/script.js
@@ -0,0 +1,11 @@
+//create a function that welcomes a user onLoad//
+/*
+function welcomeMsg() {
+    alert("Welcome to my Portfolio, it's awesome to have you here!!!")
+}
+*/
+//blog alert//
+
+function blogAlert() {
+    alert("Our Blog is not online yet, please try again later!!!");
+}
